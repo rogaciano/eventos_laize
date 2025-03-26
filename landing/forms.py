4967d35f7c3e2447d14bced51a38989b179b8120
rogaciano <rogaciano@gmail.com>
2025-03-26 09:56:41 -0300

    corrigido entre em contato

--- landing/forms.py
+++ landing/forms.py
@@ -4,13 +4,13 @@ class ContactForm(forms.Form):
     name = forms.CharField(
         max_length=100,
         widget=forms.TextInput(attrs={
-            'class': 'mt-1 block w-full rounded-md border-gray-300 border-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 bg-white',
+            'class': 'mt-1 block w-full rounded-md border-gray-300 border-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 bg-white text-black',
             'placeholder': 'Seu nome'
         })
     )
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
-            'class': 'mt-1 block w-full rounded-md border-gray-300 border-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 bg-white',
+            'class': 'mt-1 block w-full rounded-md border-gray-300 border-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 bg-white text-black',
             'placeholder': 'Seu e-mail'
         })
     )
@@ -18,7 +18,7 @@ class ContactForm(forms.Form):
         max_length=20,
         required=False,
         widget=forms.TextInput(attrs={
-            'class': 'mt-1 block w-full rounded-md border-gray-300 border-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 bg-white',
+            'class': 'mt-1 block w-full rounded-md border-gray-300 border-2 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 bg-white text-black',
             'placeholder': 'Seu telefone'
         })
     )
